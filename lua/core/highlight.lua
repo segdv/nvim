f2c3e9bbe7195969a58265b9b56c0a482e0124af
SEG <segdev.mx@gmail.com>
2025-05-04 13:33:22 -0600

    feat(highlight): add red highlights for long lines and long comments

--- lua/core/highlight.lua
+++ lua/core/highlight.lua
@@ -1,5 +1,13 @@
 local M = {};
 
+-- Highlight text beyond 100 characters in red
+vim.fn.matchadd("LineOverflow", "\\%101v.\\+", 100);
+vim.api.nvim_set_hl(0, "LineOverflow", { bg = "#aa0000" });
+
+-- Highlight text beyond 78 characters in comments (after '//') in red
+vim.fn.matchadd("CommentOverflow", "//.\\{78\\}\\zs.\\{1,}", 101);
+vim.api.nvim_set_hl(0, "CommentOverflow", { bg = "#aa0000" });
+
 -- Utility to toggle cursor visibility
 vim.api.nvim_set_hl(0, "NoCursor", { blend = 100, ctermfg = 0 });
 
