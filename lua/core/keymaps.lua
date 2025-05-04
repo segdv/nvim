2de7eddfc3c06dc9d67732a17083261dbfa44e0a
SEG <segdev.mx@gmail.com>
2025-05-04 11:47:49 -0600

    feat(keymaps): add window navigation mappings using leader and h/t/n/s

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,3 +7,8 @@ set("n", "<leader>;", "$a;<Esc>");
 set("n", "<leader>w", "<cmd>w<cr>");
 set("n", "<leader>q", "<cmd>q<cr>");
 set("n", "<c-d>", "<cmd>q<cr>");
+
+set("n", "<leader>h", "<c-w>h");
+set("n", "<leader>t", "<c-w>j");
+set("n", "<leader>n", "<c-w>k");
+set("n", "<leader>s", "<c-w>l");
