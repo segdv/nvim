cda6c3f2de592398132b4d0f20ad1d6b8401c151
SEG <segdev.mx@gmail.com>
2025-05-05 01:09:06 -0600

    feat(lsp): configure diagnostics with rounded border, disable signs, and enable virtual text

--- lua/lsp/diagnostics.lua
+++ lua/lsp/diagnostics.lua
@@ -1,5 +1,14 @@
 vim.opt.updatetime = 100;
 
+vim.diagnostic.config({
+  float = {
+    border = "rounded",
+    header = "",
+  },
+  signs = false,
+  virtual_text = true,
+});
+
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
   pattern = "*",
   callback = function()
