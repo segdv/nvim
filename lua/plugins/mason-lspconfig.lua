bcbdd6a3f7104d957bf7bf8c571d583bb3e05ac6
SEG <segdev.mx@gmail.com>
2025-05-04 19:08:49 -0600

    feat(lsp): add LSP capabilities integration with cmp_nvim_lsp

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -16,6 +16,7 @@ return {
     require("mason-lspconfig").setup_handlers({
       function(server_name)
         require("lspconfig")[server_name].setup({
+          capabilities = require("lsp.capabilities"),
         });
       end,
     });
