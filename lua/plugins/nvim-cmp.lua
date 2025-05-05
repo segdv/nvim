98fe5601f5364dcfbbc1a6a0116a07fa257f6ec7
SEG <segdev.mx@gmail.com>
2025-05-05 01:08:36 -0600

    feat(plugin): add bordered windows and customize Pmenu highlights for nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -11,6 +11,11 @@ return {
     local cmp = require("cmp");
 
     cmp.setup({
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
       mapping = {
         ["<down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -26,5 +31,8 @@ return {
         { name = "path" },
       },
     });
+
+    vim.api.nvim_set_hl(0, "Pmenu", { blend = 0 });
+    vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#444444", blend = 0 });
   end,
 };
