eeeafea7d9823d14ff91f7bb58d4dd30b33c57b5
SEG <segdev.mx@gmail.com>
2025-05-04 23:50:59 -0600

    feat(plugin): add nvim-treesitter-context for enhanced code context display

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,17 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
 
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
+
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = { "java" },
     });
+
+    vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "grey" });
+    vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "grey" });
+    vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "none" });
   end,
 };
