b493f84b0dfb55880c0e4f965837a0ba88518880
SEG <segdev.mx@gmail.com>
2025-05-04 19:03:20 -0600

    feat(plugin): add mason-lspconfig with automatic LSP server installation and default setup handler

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,23 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+  },
+
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = {},
+      automatic_installation = true,
+    });
+
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        require("lspconfig")[server_name].setup({
+        });
+      end,
+    });
+  end,
+};
